35f856f33c977577a8288fb245aa8cc481a5690e
siike92 <siike92@hotmail.com>
2025-04-06 14:36:19 -0500

    Added SDRAM functionality, still a work-in-progress and currently broken

--- target/CM7/main.cpp
+++ target/CM7/main.cpp
@@ -18,6 +18,90 @@
 // #define EFFECT1_BUTTON_PIN 		GPIO_PIN::PIN_0
 // #define EFFECT2_BUTTON_PIN 		GPIO_PIN::PIN_1
 // #define LOGGING_USART_NUM 		USART_NUM::USART_2
+#define SDRAM_CLK_PORT 			GPIO_PORT::G
+#define SDRAM_CLK_PIN 			GPIO_PIN::PIN_8
+#define SDRAM_NE0_PORT 			GPIO_PORT::C
+#define SDRAM_NE0_PIN 			GPIO_PIN::PIN_4
+#define SDRAM_CKE0_PORT 		GPIO_PORT::C
+#define SDRAM_CKE0_PIN 			GPIO_PIN::PIN_5
+#define SDRAM_NE1_PORT 			GPIO_PORT::H
+#define SDRAM_NE1_PIN 			GPIO_PIN::PIN_6
+#define SDRAM_CKE1_PORT 		GPIO_PORT::H
+#define SDRAM_CKE1_PIN 			GPIO_PIN::PIN_7
+#define SDRAM_CAS_PORT 			GPIO_PORT::G
+#define SDRAM_CAS_PIN 			GPIO_PIN::PIN_15
+#define SDRAM_RAS_PORT 			GPIO_PORT::F
+#define SDRAM_RAS_PIN 			GPIO_PIN::PIN_11
+#define SDRAM_WE_PORT 			GPIO_PORT::C
+#define SDRAM_WE_PIN 			GPIO_PIN::PIN_0
+#define SDRAM_NBL0_PORT 		GPIO_PORT::E
+#define SDRAM_NBL0_PIN 			GPIO_PIN::PIN_0
+#define SDRAM_NBL1_PORT 		GPIO_PORT::E
+#define SDRAM_NBL1_PIN 			GPIO_PIN::PIN_1
+#define SDRAM_BA0_PORT 			GPIO_PORT::G
+#define SDRAM_BA0_PIN 			GPIO_PIN::PIN_4
+#define SDRAM_BA1_PORT 			GPIO_PORT::G
+#define SDRAM_BA1_PIN 			GPIO_PIN::PIN_5
+#define SDRAM_A0_PORT 			GPIO_PORT::F
+#define SDRAM_A0_PIN 			GPIO_PIN::PIN_0
+#define SDRAM_A1_PORT 			GPIO_PORT::F
+#define SDRAM_A1_PIN 			GPIO_PIN::PIN_1
+#define SDRAM_A2_PORT 			GPIO_PORT::F
+#define SDRAM_A2_PIN 			GPIO_PIN::PIN_2
+#define SDRAM_A3_PORT 			GPIO_PORT::F
+#define SDRAM_A3_PIN 			GPIO_PIN::PIN_3
+#define SDRAM_A4_PORT 			GPIO_PORT::F
+#define SDRAM_A4_PIN 			GPIO_PIN::PIN_4
+#define SDRAM_A5_PORT 			GPIO_PORT::F
+#define SDRAM_A5_PIN 			GPIO_PIN::PIN_5
+#define SDRAM_A6_PORT 			GPIO_PORT::F
+#define SDRAM_A6_PIN 			GPIO_PIN::PIN_12
+#define SDRAM_A7_PORT 			GPIO_PORT::F
+#define SDRAM_A7_PIN 			GPIO_PIN::PIN_13
+#define SDRAM_A8_PORT 			GPIO_PORT::F
+#define SDRAM_A8_PIN 			GPIO_PIN::PIN_14
+#define SDRAM_A9_PORT 			GPIO_PORT::F
+#define SDRAM_A9_PIN 			GPIO_PIN::PIN_15
+#define SDRAM_A10_PORT 			GPIO_PORT::G
+#define SDRAM_A10_PIN 			GPIO_PIN::PIN_0
+#define SDRAM_A11_PORT 			GPIO_PORT::G
+#define SDRAM_A11_PIN 			GPIO_PIN::PIN_1
+#define SDRAM_D0_PORT 			GPIO_PORT::D
+#define SDRAM_D0_PIN 			GPIO_PIN::PIN_14
+#define SDRAM_D1_PORT 			GPIO_PORT::D
+#define SDRAM_D1_PIN 			GPIO_PIN::PIN_15
+#define SDRAM_D2_PORT 			GPIO_PORT::D
+#define SDRAM_D2_PIN 			GPIO_PIN::PIN_0
+#define SDRAM_D3_PORT 			GPIO_PORT::D
+#define SDRAM_D3_PIN 			GPIO_PIN::PIN_1
+#define SDRAM_D4_PORT 			GPIO_PORT::E
+#define SDRAM_D4_PIN 			GPIO_PIN::PIN_7
+#define SDRAM_D5_PORT 			GPIO_PORT::E
+#define SDRAM_D5_PIN 			GPIO_PIN::PIN_8
+#define SDRAM_D6_PORT 			GPIO_PORT::E
+#define SDRAM_D6_PIN 			GPIO_PIN::PIN_9
+#define SDRAM_D7_PORT 			GPIO_PORT::E
+#define SDRAM_D7_PIN 			GPIO_PIN::PIN_10
+#define SDRAM_D7_PORT 			GPIO_PORT::E
+#define SDRAM_D7_PIN 			GPIO_PIN::PIN_10
+#define SDRAM_D8_PORT 			GPIO_PORT::E
+#define SDRAM_D8_PIN 			GPIO_PIN::PIN_11
+#define SDRAM_D9_PORT 			GPIO_PORT::E
+#define SDRAM_D9_PIN 			GPIO_PIN::PIN_12
+#define SDRAM_D10_PORT 			GPIO_PORT::E
+#define SDRAM_D10_PIN 			GPIO_PIN::PIN_13
+#define SDRAM_D11_PORT 			GPIO_PORT::E
+#define SDRAM_D11_PIN 			GPIO_PIN::PIN_14
+#define SDRAM_D12_PORT 			GPIO_PORT::E
+#define SDRAM_D12_PIN 			GPIO_PIN::PIN_15
+#define SDRAM_D13_PORT 			GPIO_PORT::D
+#define SDRAM_D13_PIN 			GPIO_PIN::PIN_8
+#define SDRAM_D14_PORT 			GPIO_PORT::D
+#define SDRAM_D14_PIN 			GPIO_PIN::PIN_9
+#define SDRAM_D15_PORT 			GPIO_PORT::D
+#define SDRAM_D15_PIN 			GPIO_PIN::PIN_10
+
+#define SDRAM1_MEM_START 		0xC0000000
 
 // these pins are unconnected on SIGIL Rev 2 development board, so we disable them as per the ST recommendations
 void disableUnusedPIns()
@@ -30,21 +114,67 @@ int main(void)
 {
 	// setup clock 480MHz (also prescales peripheral clocks to fit rate limitations)
 	LLPD::rcc_clock_start_max_cpu1();
+	LLPD::rcc_start_pll2();
 
 	// enable gpio clocks
 	// LLPD::gpio_enable_clock( GPIO_PORT::A );
 	// LLPD::gpio_enable_clock( GPIO_PORT::B );
-	// LLPD::gpio_enable_clock( GPIO_PORT::C );
-	// LLPD::gpio_enable_clock( GPIO_PORT::D );
-	// LLPD::gpio_enable_clock( GPIO_PORT::E );
-	// LLPD::gpio_enable_clock( GPIO_PORT::F );
-	// LLPD::gpio_enable_clock( GPIO_PORT::G );
-	// LLPD::gpio_enable_clock( GPIO_PORT::H );
+	LLPD::gpio_enable_clock( GPIO_PORT::C );
+	LLPD::gpio_enable_clock( GPIO_PORT::D );
+	LLPD::gpio_enable_clock( GPIO_PORT::E );
+	LLPD::gpio_enable_clock( GPIO_PORT::F );
+	LLPD::gpio_enable_clock( GPIO_PORT::G );
+	LLPD::gpio_enable_clock( GPIO_PORT::H );
+
+	// setup sdram pins
+	LLPD::gpio_output_setup( SDRAM_CLK_PORT, SDRAM_CLK_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_NE0_PORT, SDRAM_NE0_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_CKE0_PORT, SDRAM_CKE0_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_NE1_PORT, SDRAM_NE1_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_CKE1_PORT, SDRAM_CKE1_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_CAS_PORT, SDRAM_CAS_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_RAS_PORT, SDRAM_RAS_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_WE_PORT, SDRAM_WE_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_NBL0_PORT, SDRAM_NBL0_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_NBL1_PORT, SDRAM_NBL1_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_BA0_PORT, SDRAM_BA0_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_BA1_PORT, SDRAM_BA1_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A0_PORT, SDRAM_A0_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A1_PORT, SDRAM_A1_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A2_PORT, SDRAM_A2_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A3_PORT, SDRAM_A3_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A4_PORT, SDRAM_A4_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A5_PORT, SDRAM_A5_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A6_PORT, SDRAM_A6_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A7_PORT, SDRAM_A7_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A8_PORT, SDRAM_A8_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A9_PORT, SDRAM_A9_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A10_PORT, SDRAM_A10_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_A11_PORT, SDRAM_A11_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D0_PORT, SDRAM_D0_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D1_PORT, SDRAM_D1_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D2_PORT, SDRAM_D2_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D3_PORT, SDRAM_D3_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D4_PORT, SDRAM_D4_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D5_PORT, SDRAM_D5_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12);
+	LLPD::gpio_output_setup( SDRAM_D6_PORT, SDRAM_D6_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12);
+	LLPD::gpio_output_setup( SDRAM_D7_PORT, SDRAM_D7_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12);
+	LLPD::gpio_output_setup( SDRAM_D8_PORT, SDRAM_D8_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12);
+	LLPD::gpio_output_setup( SDRAM_D9_PORT, SDRAM_D9_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12);
+	LLPD::gpio_output_setup( SDRAM_D10_PORT, SDRAM_D10_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D11_PORT, SDRAM_D11_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D12_PORT, SDRAM_D12_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D13_PORT, SDRAM_D13_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D14_PORT, SDRAM_D14_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
+	LLPD::gpio_output_setup( SDRAM_D15_PORT, SDRAM_D15_PIN, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::VERY_HIGH, true, 12 );
 
 	// USART setup
 	// LLPD::usart_init( LOGGING_USART_NUM, USART_WORD_LENGTH::BITS_8, USART_PARITY::NONE, USART_CONF::TX_AND_RX,
 	// 			USART_STOP_BITS::BITS_1, 120000000, 9600 );
 	// LLPD::usart_log( LOGGING_USART_NUM, "SIGIL starting up ----------------------------" );
+
+	// timer setup (for 30 kHz sampling rate at 480 MHz / 2 timer clock)
+	LLPD::tim6_counter_setup( 0, 8000, 30000 );
 
 	// adc setup (note this must be done after the tim6_counter_start() call since it uses the delay funtion)
 	// LLPD::gpio_analog_setup( EFFECT_ADC_PORT, EFFECT1_ADC_PIN );
@@ -62,27 +192,33 @@ int main(void)
 	// LLPD::gpio_digital_input_setup( EFFECT_BUTTON_PORT, EFFECT1_BUTTON_PIN, GPIO_PUPD::PULL_UP );
 	// LLPD::gpio_digital_input_setup( EFFECT_BUTTON_PORT, EFFECT2_BUTTON_PIN, GPIO_PUPD::PULL_UP );
 
+	// sdram setup
+	LLPD::fmc_sdram_init( FMC_SDRAM_BANK::BANK_5, FMC_SDRAM_COL_ADDR_BITS::BITS_8, FMC_SDRAM_ROW_ADDR_BITS::BITS_12,
+			FMC_SDRAM_DATA_ADDR_BITS::BITS_16, FMC_SDRAM_NUM_BANKS::BANKS_4, FMC_SDRAM_CLOCK_CONFIG::CYCLES_2,
+			FMC_SDRAM_CAS_LATENCY::CYCLES_3, true, FMC_SDRAM_RPIPE_DELAY::CYCLES_2, false, 2, 11, 7, 9, 4, 3, 3 );
+	LLPD::fmc_sdram_start( true, false, 2, 64, 4096, 150, 0 | (0b011 << 4) | (0b1 << 9) );
+
 	// LLPD::usart_log( LOGGING_USART_NUM, "SIGIL setup complete, entering while loop -------------------------------" );
 
 	// flush denormals
 	__set_FPSCR( __get_FPSCR() | (1 << 24) );
 
 	// enable instruction cache
-	SCB_EnableICache();
+	// SCB_EnableICache();
 
 	// enable data cache (will only be useful for constant values stored in flash)
-	SCB_InvalidateDCache();
-	SCB_EnableDCache();
+	// SCB_InvalidateDCache();
+	// SCB_EnableDCache();
 
-	volatile int someValue = 0;
+	uint8_t* someValue = (uint8_t*) SDRAM1_MEM_START;
 
 	while ( true )
 	{
-		someValue += 2;
+		*someValue += 2;
 
-		if ( someValue >= 60 )
+		if ( *someValue >= 60 )
 		{
-			someValue -=4;
+			*someValue -=4;
 		}
 
 		// LLPD::adc_perform_conversion_sequence( EFFECT_ADC_NUM );
